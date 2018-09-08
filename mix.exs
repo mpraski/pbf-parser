d25f607718d61de95181b11e120cf030a5de5676
mpraski <marcin.praski@live.com>
2018-09-08 11:30:07 +0100

    edited readme

--- mix.exs
+++ mix.exs
@@ -7,25 +7,38 @@ defmodule PBFParser.MixProject do
       version: "0.1.0",
       elixir: "> 1.7.2",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "PBF Parser",
+      source_url: "https://github.com/mpraski/pbf-parser"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:exprotobuf, "~> 1.2"},
       {:flow, "~> 0.14.2"},
       {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Elixir parser and decoder for OpenStreetMap PBF format described in PBF file specification."
+  end
+
+  defp package() do
+    [
+      files: ~w(lib config .formatter.exs mix.exs README* LICENSE*
+                CHANGELOG*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mpraski/pbf-parser"}
     ]
   end
 end
